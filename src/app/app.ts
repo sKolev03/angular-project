2fcac39e2c4abf53ae13eabbd67b0669105b9ccf
sKolev03 <stiliyankolev03@gmail.com>
2026-04-21 01:14:27 +0300

    Add app footer component
    Create a new Footer component. Add <app-footer> to app.html and register Footer in app.ts. Update styles.css to add a temporary top margin for the footer.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
