583df5799560ba7ad5912b5b8e1dcd25576bdfa1
sKolev03 <stiliyankolev03@gmail.com>
2026-04-21 01:39:22 +0300

    Add Home component
    Create a new Home feature. Integrate the component into the root app and import Home in app.ts. Also remove a temporary footer margin-top from styles.css.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { Home } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
