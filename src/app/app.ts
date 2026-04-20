b6bbb6f0a8572935c6ebc503dab496e240fd9950
sKolev03 <stiliyankolev03@gmail.com>
2026-04-21 00:34:46 +0300

    Add Header component and update styles
    Create a new Header component and include it in the app. Update global styles, fix import paths - src/style.css.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
