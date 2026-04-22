a48b6adfd6162a705d09b73442991e574a9c030a
sKolev03 <stiliyankolev03@gmail.com>
2026-04-22 19:55:05 +0300

    Add User interfaces, UserService and Auth
    Create User and UserWithCredentials interfaces and an in-memory UserService with register and validateCredentials. Implement Auth service using Angular signals/computed to track currentUser and isLoggedIn, with login, setSession and logout methods.

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,6 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { UserService } from './user';
+import { User } from '../../shared/interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  private userService = inject(UserService);
+  private user = signal<User | null>(null);
+
+  isLoggedIn = computed(() => this.user() !== null);
+  currentUser = computed(() => this.user());
+
+  login(email: string, passsword: string): boolean {
+    const isUser = this.userService.validateCredentials(email, passsword);
+
+    if (!isUser) {
+      return false;
+    }
+
+    this.user.set(isUser);
+    return true;
+  }
+
+  setSession(user: User): void {
+    this.user.set(user);
+  }
+
+  logout(): void {
+    this.user.set(null);
+  }
+}
