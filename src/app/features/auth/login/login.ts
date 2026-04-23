16818f21d09f0ca156e4efcd46ff4bec821d32ff
sKolev03 <stiliyankolev03@gmail.com>
2026-04-23 18:23:40 +0300

    Revamp auth login/register UI and add login logic
    Modernize and restyle the authentication forms and add basic login behavior. Implemented login.ts to inject AuthService and Router, and add an onLogin() handler that attempts authentication and navigates to /themes on success (alerts on failure).

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  email = '';
+  password = '';
+
+  onLogin(): void {
+    const success = this.authService.login(this.email, this.password);
+
+    if (success) {
+      this.router.navigate(['/themes']);
+    } else {
+      alert('Invalid email or password');
+    }
+  }
+}
