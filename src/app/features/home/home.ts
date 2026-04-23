2572087171116608b533a368fd831e164fbde9e0
sKolev03 <stiliyankolev03@gmail.com>
2026-04-23 19:26:04 +0300

    Unify page background and enhance home UI

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { AuthService } from '../../core/services/auth';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  
+  private authService = inject(AuthService);
+
+  isLoggedIn = this.authService.isLoggedIn;
 }
